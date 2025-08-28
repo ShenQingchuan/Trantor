f6c902419b35bb3a5687bd16283ba38256135e5b
ShenQingchuan <tmy991106@gmail.com>
2025-08-28 12:27:09 +0800

    fix: 回退错误修改

--- src/server/middlewares/llmClient.ts
+++ src/server/middlewares/llmClient.ts
@@ -2,16 +2,12 @@ import { env } from 'node:process'
 import OpenAI from 'openai'
 import { createTrantorMiddleware } from '../utils/setup.js'
 
-let llmClient: OpenAI | null = null
-
 export const llmClientMiddleware = createTrantorMiddleware(async (c, next) => {
-  if (!llmClient) {
-    llmClient = new OpenAI({
-      apiKey: env.LLM_API_KEY,
-      baseURL: env.LLM_BASE_URL,
-    })
-    c.set('llmClient', llmClient)
-  }
+  const llmClient = new OpenAI({
+    apiKey: env.LLM_API_KEY,
+    baseURL: env.LLM_BASE_URL,
+  })
+  c.set('llmClient', llmClient)
 
   await next()
 })
